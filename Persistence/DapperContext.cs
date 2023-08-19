984739c402257975dfc5bd9a92b34f9917d8ba81
M.Asadollahi <asadolahi.ma7@gmail.com>
2023-08-19 18:36:11 +0430

    Add get all students with dapper

---
+++ Persistence/DapperContext.cs
@@ -0,0 +1,28 @@
+﻿
+
+using Dapper;
+using System.Data.SqlClient;
+
+namespace Persistence;
+
+public class DapperContext : IDbContext
+{
+    private string _connectionString;
+
+    public DapperContext()
+    {
+        _connectionString = "Data Source=M-A7; Initial Catalog=SchoolDb; Integrated Security=SSPI";
+
+    }
+    public void ExecuteCommand(string sql)
+    {
+        using var conn = new SqlConnection(_connectionString);
+        conn.Execute(sql);
+    }
+
+    public IEnumerable<T> ExecuteQuery<T>(string sql)
+    {
+        using var conn = new SqlConnection(_connectionString);
+        return conn.Query<T>(sql);
+    }
+}
